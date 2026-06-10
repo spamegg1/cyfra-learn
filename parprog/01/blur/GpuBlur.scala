16816a4f726bd1d5dc605d7b97882f5a21b62f47
spamegg <spamegg@pm.me>
2026-06-10 10:02:41 +0300

    finish gpu blur, but it crashes at runtime

--- parprog/01/blur/GpuBlur.scala
+++ parprog/01/blur/GpuBlur.scala
@@ -45,14 +45,14 @@ object GpuBlur:
     GIO
       .repeat(xmax - xmin): i =>
         GIO.repeat(ymax - ymin): j =>
-          val neighbor = layout.in.read(j * w + i)
+          val neighbor = layout.in.read((j + ymin) * w + (i + xmin))
           count += 1
           /* add neighbor's Rgba values to accumulated 4 channels */
           rnew += neighbor.red
           gnew += neighbor.green
           bnew += neighbor.blue
           anew += neighbor.alpha
-          ??? // what goes here? () does not work
+          GIO.printf("") // placeholder
       .flatMap: _ =>
         val blurred = rgba(rnew / count, gnew / count, bnew / count, anew / count)
         GIO.write(layout.out, idx, blurred)
